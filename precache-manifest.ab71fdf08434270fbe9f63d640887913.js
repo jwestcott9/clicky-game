90dc547e5353fcb6ef05e507d99e4bd6aa2a5d51
jwestcott9 <jwestcott9@gmail.com>
2019-08-20 22:37:22 -0600

    Updates

---
+++ precache-manifest.ab71fdf08434270fbe9f63d640887913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efff1a43d906a5135f62d3a8dd4f9fe4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f3d08bec18dc8bf90aa4",
+    "url": "/clicky-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9de46ad4b3b8f8d2bd67",
+    "url": "/clicky-game/static/css/main.ac49232a.chunk.css"
+  },
+  {
+    "revision": "f3d08bec18dc8bf90aa4",
+    "url": "/clicky-game/static/js/2.5225e144.chunk.js"
+  },
+  {
+    "revision": "9de46ad4b3b8f8d2bd67",
+    "url": "/clicky-game/static/js/main.86313075.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
